52c8ee1edaf79076cb69f8b274a34e55b4e56d6b
angemissiba <as.missiba@b2i-solutions.com>
2024-10-22 09:05:29 +0200

    #v1

---
+++ src/components/web/home/HomeValue.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+const HomeValueSection = () => {
+  return (          
+    <div class="section-full bg-info">
+    <div class="container">
+         <div class="section-content ">
+            <div class="wt-subscribe-box">
+                <div class="container">
+                    <div class="row call-to-action-wrap">
+                        <div class="col-lg-9 col-md-12">
+                            <div class="call-to-action-left ">
+                                <h4 class="text-uppercase m-b10 font-weight-600">Invest in Cryptocurrency Bitcoin Mining & Easy Way to Trade Bitcoin.</h4>
+                                <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore edolore magna aliquyam erat.</p>
+                            </div>
+                        </div>
+                        
+                        <div class="col-lg-3 col-md-12">
+                            <div class="call-to-action-right">
+                                <a href="#" class="site-button-secondry bg-primary text-uppercase font-weight-600">
+                                    Read More 
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+ </div>
+  );
+}
+
+export default HomeValueSection;
--- src/pages/web/HomePage.js
+++ src/pages/web/HomePage.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import HomeSlider from '../../components/web/home/HomeSlider'
+import HomeMarqueScroll from '../../components/web/home/HomeMarqueScroll'
+import HomeValueSection from '../../components/web/home/HomeValue'
 
 const HomePage = () => {
   return (
   <div class="page-content">
       <HomeSlider/>
+      <HomeMarqueScroll/>
+      <HomeValueSection/>
     </div>
   )
 }
